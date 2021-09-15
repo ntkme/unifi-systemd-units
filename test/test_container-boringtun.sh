a1e39700c9f48d99ecb5f676a17bf4f9923e8af0
なつき <i@ntk.me>
2021-09-15 16:52:58 -0700

    Update container-boringtun@.service

--- test/test_container-boringtun.sh
+++ test/test_container-boringtun.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /mnt/data/etc/wireguard
 
+( umask 077 && printf '%s\n' '[Interface]' 'Address = 10.8.0.1/24' 'PostUp = iptables --table nat --append POSTROUTING --jump MASQUERADE ' 'PostDown = iptables --table nat --delete POSTROUTING --jump MASQUERADE' 'ListenPort = 51820' 'PrivateKey = SJ5y5piShMk1OW34BfUrwh6duBPbV6NIngtJQvhFd34=' | tee /mnt/data/etc/wireguard/wg0.conf )
+
 podman exec unifi-systemd systemctl start container-boringtun@wg0.service
 
 sleep 1
